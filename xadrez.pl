d22e73fc6b4b9499a10a6d339ecda060f712067b
Ricardo <l42647@alunos.uevora.pt>
2023-01-09 05:10:52 +0000

    add: Board and pieces rules

--- xadrez.pl
+++ xadrez.pl
@@ -1,45 +1,108 @@
-:- initialization(command).
+:- initialization(print_board).
 
-command :- argument_list(As), interpreter(As).
+command :- argument_list(As), command(As).
 
-interpreter([]).
-interpreter([A|As]) :- arg(A), gets(As).
+formatos(algebrica, descritiva, postal).
+acoes(algebrica, descritiva, postal, mostrar, estado).
+
+is_algebrica(A) :- member(A, formatos).
+is_descritiva(A) :- nth(2, formatos, A).
+is_postal(A) :- nth(3, formatos, A).
+is_mostrar(S) :- nth(4, acoes, S).
+is_estado(S) :- nth(5, acoes, S).
+
+command([]).
+command([A|As]) :- arg(A), command(As).
 
 arg('-') :- !, format("\n[standard input]\n\n", []).
 
-gets(As) :- get0(C), gets([], C, As).
+arg(A) :- format("\n[formato ~w]\n\n", [A]).
 
-gets(As, 10, As).		% 10 é o newline
-gets(As, -1, As).		% -1 é o end-of-file
-gets(I, C, [C|O]) :- get0(CC), gets(I, CC, O).
+arg(B) :- is_mostrar(B), is_estado(B), !,
+        format("\n[acao ~w]\n\n", [B]).
 
+arg(F) :- file_exists(F), see(F), !,
+		format("\n[file ~w]\n\n", [F]).
 
+arg(U) :- format("[nao existe: ~w]\n", [U]), !, halt.
 
-/* xadrez :- argument_list(As), comando(As).. */
+% define the chess board with pieces at start positions
+board([
+        ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+        ['es', 'es', 'es', 'es', 'es', 'es', 'es', 'es'],
+        ['es', 'es', 'es', 'es', 'es', 'es', 'es', 'es'],
+        ['es', 'es', 'es', 'es', 'es', 'es', 'es', 'es'],
+        ['es', 'es', 'es', 'es', 'es', 'es', 'es', 'es'],
+        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+        ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR']
+      ]).
 
-/* formato_jogadas(As) :- nth0(1, As, Formato),
-    string_codes(FormatoStr, Formato),
-    (FormatoStr == 'algebrica' ->
-    true
-    ; FormatoStr == 'descritiva' ->
-    true
-    ; FormatoStr == 'postal' ->
-    true
-    ; write('Formato de jogadas inválido'), nl, fail).
-    
-ações(As) :- nth0(2, As, Ação1),
-    string_codes(Ação1Str, Ação1),
-    (Ação1Str == 'algebrica' ->
-    true
-    ; Ação1Str == 'descritiva' ->
-    true
-    ; Ação1Str == 'postal' ->
-    true
-    ; Ação1Str == 'mostrar' ->
-    true
-    ; Ação1Str == 'estado' ->
-    true
-    ; write('Ação inválida'), nl, fail). */
+% print the board to the console
+print_board :-
+        board(B),
+        print_board(B).
+      
+      print_board([]) :- nl.
+      print_board([Row|Rest]) :-
+        print_row(Row),
+        print_board(Rest).
+      
+      print_row(Row) :-
+        write('|'),
+        print_row(Row, '|'),
+        nl.
+      
+      print_row([], _) :- !.
+      print_row([Piece|Rest], Separator) :-
+        write(Separator), write(Piece),
+        print_row(Rest, Separator).
 
-main :- true.
+chess_rules([piece_color, 80], X, Y, X1, Y1) :- % Pawn
+(Y1 is Y+1 ; Y1 is Y+2),
+X1 is X,
+(Y1 =:= 2 ; Y1 =:= 7), % First move (can move two squares)
+\+ occupied(X1, Y1).
+chess_rules([piece_color, 80], X, Y, X1, Y1) :- % Pawn
+Y1 is Y+1,
+X1 is X,
+\+ occupied(X1, Y1).
+chess_rules([piece_color, 80], X, Y, X1, Y1) :- % Pawn
+Y1 is Y+1,
+(X1 is X+1 ; X1 is X-1),
+occupied(X1, Y1).
 
+chess_rules([piece_color, 82]X, Y, X1, Y1) :- % Rook
+(X1 =:= X ; Y1 =:= Y),
+\+ occupied(X1, Y1).
+
+chess_rules([peice_color, 78], X, Y, X1, Y1) :- % Knight
+(X1 is X+2 ; X1 is X-2),
+(Y1 is Y+1 ; Y1 is Y-1),
+\+ occupied(X1, Y1).
+chess_rules([piece_color, 78]X, Y, X1, Y1) :- % Knight
+(X1 is X+1 ; X1 is X-1),
+(Y1 is Y+2 ; Y1 is Y-2),
+\+ occupied(X1, Y1).
+
+chess_rules([piece_color, 66], X, Y, X1, Y1) :- % Bishop
+X1 is X + Y1 - Y,
+Y1 is Y + X1 - X,
+\+ occupied(X1, Y1).
+
+chess_rules([piece_color, 81], X, Y, X1, Y1) :- % Queen
+X1 is X + Y1 - Y,
+Y1 is Y + X1 - X,
+\+ occupied(X1, Y1).
+
+chess_rules([piece_color, 81]X, Y, X1, Y1) :- % Queen
+(X1 =:= X ; Y1 =:= Y),
+\+ occupied(X1, Y1).
+
+chess_rules([piece_color, 75], X, Y, X1, Y1) :- % King
+(X1 is X+1 ; X1 is X-1 ; X1 is X),
+(Y1 is Y+1 ; Y1 is Y-1 ; Y1 is Y),
+\+ occupied(X1, Y1).
+
+occupied(X, Y) :- occupied(board, X, Y).
+occupied(B, X, Y) :- (X =:= 0, Y =:= 0; X =:= 8, Y =:= 8, X1 =:= 8).
