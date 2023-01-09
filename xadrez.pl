317318d07ab7c7db0a66f2ef8dc4ff1950a1fee3
Ricardo <l42647@alunos.uevora.pt>
2023-01-09 06:00:15 +0000

    update: Game

--- xadrez.pl
+++ xadrez.pl
@@ -1,30 +1,30 @@
-:- initialization(print_board).
+:- initialization(start_game).
 
-command :- argument_list(As), command(As).
+start_game :- argument_list(As), verify_format(As).
 
-formatos(algebrica, descritiva, postal).
-acoes(algebrica, descritiva, postal, mostrar, estado).
+is_algebrica('algebrica') :- write('formato: algebrica'), nl, !.
+is_descritiva('descritiva') :- write('formato: descritiva'), nl, !.
+is_postal('postal') :- write('formato: postal'), nl, !.
+is_mostrar('mostrar') :- print_board.
+is_estado('estado') :- verify_check.
 
-is_algebrica(A) :- member(A, formatos).
-is_descritiva(A) :- nth(2, formatos, A).
-is_postal(A) :- nth(3, formatos, A).
-is_mostrar(S) :- nth(4, acoes, S).
-is_estado(S) :- nth(5, acoes, S).
+verify_format([F|T]) :- (is_algebrica(F); is_descritiva(F); is_postal(F)), verify_action(T).
+verify_action([A|T]) :- (is_mostrar(A); is_estado(A)), start_game(T).
 
-command([]).
+start_game(B) :- print_board.
+/* command([]).
 command([A|As]) :- arg(A), command(As).
 
 arg('-') :- !, format("\n[standard input]\n\n", []).
 
-arg(A) :- format("\n[formato ~w]\n\n", [A]).
+arg(F) :- is_algebrica(F); is_descritiva(F); is_postal(F).
 
-arg(B) :- is_mostrar(B), is_estado(B), !,
-        format("\n[acao ~w]\n\n", [B]).
+arg(B) :- is_mostrar(B); is_estado(B), !.
 
 arg(F) :- file_exists(F), see(F), !,
 		format("\n[file ~w]\n\n", [F]).
 
-arg(U) :- format("[nao existe: ~w]\n", [U]), !, halt.
+arg(U) :- format("[nao existe: ~w]\n", [U]), !, halt. */
 
 % define the chess board with pieces at start positions
 board([
@@ -58,6 +58,8 @@ print_board :-
         write(Separator), write(Piece),
         print_row(Rest, Separator).
 
+verify_check :- write('teste'), nl.
+
 chess_rules([piece_color, 80], X, Y, X1, Y1) :- % Pawn
 (Y1 is Y+1 ; Y1 is Y+2),
 X1 is X,
@@ -72,7 +74,7 @@ Y1 is Y+1,
 (X1 is X+1 ; X1 is X-1),
 occupied(X1, Y1).
 
-chess_rules([piece_color, 82]X, Y, X1, Y1) :- % Rook
+chess_rules([piece_color, 82], X, Y, X1, Y1) :- % Rook
 (X1 =:= X ; Y1 =:= Y),
 \+ occupied(X1, Y1).
 
@@ -80,7 +82,7 @@ chess_rules([peice_color, 78], X, Y, X1, Y1) :- % Knight
 (X1 is X+2 ; X1 is X-2),
 (Y1 is Y+1 ; Y1 is Y-1),
 \+ occupied(X1, Y1).
-chess_rules([piece_color, 78]X, Y, X1, Y1) :- % Knight
+chess_rules([piece_color, 78], X, Y, X1, Y1) :- % Knight
 (X1 is X+1 ; X1 is X-1),
 (Y1 is Y+2 ; Y1 is Y-2),
 \+ occupied(X1, Y1).
@@ -95,7 +97,7 @@ X1 is X + Y1 - Y,
 Y1 is Y + X1 - X,
 \+ occupied(X1, Y1).
 
-chess_rules([piece_color, 81]X, Y, X1, Y1) :- % Queen
+chess_rules([piece_color, 81], X, Y, X1, Y1) :- % Queen
 (X1 =:= X ; Y1 =:= Y),
 \+ occupied(X1, Y1).
 
@@ -104,5 +106,6 @@ chess_rules([piece_color, 75], X, Y, X1, Y1) :- % King
 (Y1 is Y+1 ; Y1 is Y-1 ; Y1 is Y),
 \+ occupied(X1, Y1).
 
-occupied(X, Y) :- occupied(board, X, Y).
-occupied(B, X, Y) :- (X =:= 0, Y =:= 0; X =:= 8, Y =:= 8, X1 =:= 8).
+occupied(X, Y).
+/* occupied(X, Y) :- occupied(board, X, Y). */
+/* occupied(B, X, Y) :- . */
