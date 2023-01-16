10cc7b7c89f3b20a33d990fb280b3888c4285393
Ricardo <l42647@alunos.uevora.pt>
2023-01-16 02:44:57 +0000

    update: Some progress

--- xadrez.pl
+++ xadrez.pl
@@ -2,43 +2,50 @@
 
 start :- argument_list(As), verify_format(As).
 
-is_algebrica('algebrica') :- write('formato: algebrica'), nl, !.
-is_descritiva('descritiva') :- write('formato: descritiva'), nl, !.
-is_postal('postal') :- write('formato: postal'), nl, !.
+is_algebrica('algebrica') :- write('formato: algebrica'), nl, !. % TO DO
+is_descritiva('descritiva') :- write('formato: descritiva'), nl, !. % TO DO
+is_postal('postal') :- write('formato: postal'), nl, !. % TO DO
 is_mostrar('mostrar') :- write('ação: mostrar'), nl, !. % TEMP: remover e adicionar a função final em baixo, após os moves todos
 /* is_mostrar('mostrar') :- print_board. */
-is_estado('estado') :- verify_check.
+is_estado('estado') :- verify_check. % Verify if King is in check at this Board instance
 
-verify_format([F|T]) :- (is_algebrica(F); is_descritiva(F); is_postal(F)), verify_action(T).
-verify_action([A,J|T]) :-  open_game_file(J), (is_mostrar(A); is_estado(A)).
+verify_format([F|T]) :- (is_algebrica(F); is_descritiva(F); is_postal(F)), verify_action(T). % Verify 1st argument (format)
+verify_action([A,J|T]) :-  open_game_file(J), (is_mostrar(A); is_estado(A)). % Verify 2nd argument (action)
 
-open_game_file(J) :- 
+open_game_file(J) :- % Vetify Tail (last argument) from argument_list (file to open)
   file_exists(J),see(J), start_game,
   format("\n[file ~w]\n\n", [J]).
 
-start_game :- write('esvaziou [current_move]'), nl, get0(C), check_char(C,[]).
+% Read char by char. If there is passed any [MoveList] continue to append on it. Otherwise clear the current [MoveList] (new movement found) 
+start_game :- get0(C), check_char(C,[]).
 start_game(MoveList) :- get0(C), check_char(C, MoveList).
 
-check_char(32, List) :- write('SPACE'), write(List), nl, make_move_handler(List, false), start_game. % space (new move) | 1 for white
-check_char(10, List) :- write('ENTER'), write(List), nl, make_move_handler(List, true), start_game. % enter (end of turn) | 0 for black
+check_char(32, List) :- make_move_handler(List, false), start_game. % space (new move) | 1 for white
+check_char(10, List) :- make_move_handler(List, true), start_game. % enter (end of turn) | 0 for black
 check_char(-1, List). % EOF (game finished)
-check_char(C, List) :- append(List, [C], MoveList), start_game(MoveList), !.
+check_char(C, List) :- append(List, [C], MoveList), start_game(MoveList), !. % no 'special' char found, continue adding chars to [MoveList]
 
-make_move_handler(List, PieceColor) :- 
-  write('-> make_move_handler args: '), write(List), write(' '), write(PieceColor), nl,
+make_move_handler(List, PieceColor) :- % Predicate that handles the movement lenght
   (length(List, 2), make_move_two(List, PieceColor));
   (length(List, 3), make_move_three(List, PieceColor));
   (length(List, 4), make_move_four(List, PieceColor)).
   /* (length(List, 5), make_move_five). */
 
 make_move_two([X,Y], PC):- 
-  X1 is X-96, Y1 is Y-48, write(X1), write(' '), write(Y1), nl, chess_board(Board),
+  X1 is X-96, Y1 is Y-48, chess_board(Board),
   find_pawn(Board, X1, Y1, PC, UpdatedBoard), retract(chess_board(_)), assertz(chess_board(UpdatedBoard)), print_board.
 make_move_three([X,Y,Z], PC):-
-  Y1 is Y-96, Z1 is Z-48, write(X), write(' '), write(Y1), write(' '), write(Z1), nl, chess_board(Board),
+  Y1 is Y-96, Z1 is Z-48, chess_board(Board),
   move_piece(Board, X, Y1, Z1, PC, UpdatedBoard), retract(chess_board(_)), assertz(chess_board(UpdatedBoard)), print_board.
-make_move_four([X,Y,Z,W], PC):- write('4 chars move'), nl, nl.
-/* make_move_five:- write('5 chars move'), nl, nl. */
+make_move_four([X,Y,Z,W], PC):-
+  chess_board(Board),
+  ( % The second char is 'x' so it is a take move, act like a normal move
+    Y = 120, Z1 is Z-96, W1 is W-48,
+    move_piece(Board, X, Z1, W1, PC, UpdatedBoard), retract(chess_board(_)), assertz(chess_board(UpdatedBoard)), print_board
+  ); 
+  (
+    write('Teste'), nl
+  ).
 
 find_pawn(Board, X, Y, PC, UpdatedBoard) :- 
   ( (PC = false, name(Piece, [119, 80]) ) ; (PC = true, name(Piece, [98, 80])) ),
@@ -64,7 +71,6 @@ do_the_move(Board, X, Y, PC, UpdatedBoard) :-
   
 move_piece(Board, P, X, Y, PC, UpdatedBoard) :-
   ( (PC = false, name(Piece, [119, P]) ) ; (PC = true, name(Piece, [98, P])) ),
-  write('PEÇA: '), write(Piece), nl,
   name(Piece, PieceList),
   chess_rules(PieceList, X, Y, FromX, FromY, Board),
   nth(FromY, Board, FromRow),
@@ -119,25 +125,20 @@ print_board :-
 verify_check :- write('teste'), nl.
 
 chess_rules([C, 78], X, Y, FromX, FromY, Board) :- % Knight
-  write('Knight'), nl,
   ( (X1 is X + 2) ; (X1 is X - 2) ),
   ( (Y1 is Y + 1) ; (Y1 is Y - 1) ),
   is_inside_board(X1, Y1),
   find_piece(X1, Y1, Board, [C, 78]),
-  FromX is X1, FromY is Y1,
-  write(FromX), write(' '), write(FromY), nl.
+  FromX is X1, FromY is Y1.
 
 chess_rules([C, 78], X, Y, FromX, FromY, Board) :- % Knight
-  write('Knight 2'), nl,
   ( (X1 is X + 1) ; (X1 is X - 1) ),
   ( (Y1 is Y + 2) ; (Y1 is Y - 2) ),
   is_inside_board(X1, Y1),
   find_piece(X1, Y1, Board, [C, 78]),
-  FromX is X1, FromY is Y1,
-  write(FromX), write(' '), write(FromY), nl.
+  FromX is X1, FromY is Y1.
 
 chess_rules([C, 66], X, Y, FromX, FromY, Board) :- % Bishop
-  write('Bishop'), nl,
   ( ( X1 is X + 1, Y1 is Y + 1 ); ( X1 is X + 1, Y1 is Y - 1 ); ( X1 is X - 1, Y1 is Y + 1 ); ( X1 is X - 1, Y1 is Y - 1 );
   ( X1 is X + 2, Y1 is Y + 2 ); ( X1 is X + 2, Y1 is Y - 2 ); ( X1 is X - 2, Y1 is Y + 2 ); ( X1 is X - 2, Y1 is Y - 2 );
   ( X1 is X + 3, Y1 is Y + 3 ); ( X1 is X + 3, Y1 is Y - 3 ); ( X1 is X - 3, Y1 is Y + 3 ); ( X1 is X - 3, Y1 is Y - 3 );
@@ -147,38 +148,52 @@ chess_rules([C, 66], X, Y, FromX, FromY, Board) :- % Bishop
   ( X1 is X + 7, Y1 is Y + 7 ); ( X1 is X + 7, Y1 is Y - 7 ); ( X1 is X - 7, Y1 is Y + 7 ); ( X1 is X - 7, Y1 is Y - 7 ) ),
   is_inside_board(X1, Y1),
   find_piece(X1, Y1, Board, [C, 66]),
-  FromX is X1, FromY is Y1,
-  write(FromX), write(' '), write(FromY), nl.
+  FromX is X1, FromY is Y1.
 
 chess_rules([C, 82], X, Y, FromX, FromY, Board) :- % Rook
-  write('Rook'), nl,
   between(1, 8, X1),
   between(1, 8, Y1),
   (X1 =:= X ; Y1 =:= Y),
-  write(X1), write(Y1), nl,
   is_inside_board(X1, Y1),
   find_piece(X1, Y1, Board, [C, 82]),
-  FromX is X1, FromY is Y1,
-  write(FromX), write(' '), write(FromY), nl.
+  FromX is X1, FromY is Y1.
 
 chess_rules([C, 75], X, Y, FromX, FromY, Board) :- % King
-  write('King'), nl,
   between(-1, 1, Xaux),
   between(-1, 1, Yaux),
   (X1 is X + Xaux, Y1 is Y + Yaux),
-  write(X1), write(Y1), nl,
   is_inside_board(X1, Y1),
   find_piece(X1, Y1, Board, [C, 75]),
-  FromX is X1, FromY is Y1,
-  write(FromX), write(' '), write(FromY), nl.
+  FromX is X1, FromY is Y1.
+
+chess_rules([C, 81], X, Y, FromX, FromY, Board) :- % Queen
+% Bishop like move (Diagonals)
+(
+  ( ( X1 is X + 1, Y1 is Y + 1 ); ( X1 is X + 1, Y1 is Y - 1 ); ( X1 is X - 1, Y1 is Y + 1 ); ( X1 is X - 1, Y1 is Y - 1 );
+  ( X1 is X + 2, Y1 is Y + 2 ); ( X1 is X + 2, Y1 is Y - 2 ); ( X1 is X - 2, Y1 is Y + 2 ); ( X1 is X - 2, Y1 is Y - 2 );
+  ( X1 is X + 3, Y1 is Y + 3 ); ( X1 is X + 3, Y1 is Y - 3 ); ( X1 is X - 3, Y1 is Y + 3 ); ( X1 is X - 3, Y1 is Y - 3 );
+  ( X1 is X + 4, Y1 is Y + 4 ); ( X1 is X + 4, Y1 is Y - 4 ); ( X1 is X - 4, Y1 is Y + 4 ); ( X1 is X - 4, Y1 is Y - 4 );
+  ( X1 is X + 5, Y1 is Y + 5 ); ( X1 is X + 5, Y1 is Y - 5 ); ( X1 is X - 5, Y1 is Y + 5 ); ( X1 is X - 5, Y1 is Y - 5 );
+  ( X1 is X + 6, Y1 is Y + 6 ); ( X1 is X + 6, Y1 is Y - 6 ); ( X1 is X - 6, Y1 is Y + 6 ); ( X1 is X - 6, Y1 is Y - 6 );
+  ( X1 is X + 7, Y1 is Y + 7 ); ( X1 is X + 7, Y1 is Y - 7 ); ( X1 is X - 7, Y1 is Y + 7 ); ( X1 is X - 7, Y1 is Y - 7 ) ),
+  is_inside_board(X1, Y1),
+  find_piece(X1, Y1, Board, [C, 81])
+);
+( % Rook like move
+  between(1, 8, X1),
+  between(1, 8, Y1),
+  (X1 =:= X ; Y1 =:= Y),
+  is_inside_board(X1, Y1),
+  find_piece(X1, Y1, Board, [C, 81])
+),
+FromX is X1, FromY is Y1.
 
 find_piece(X, Y, Board, Piece) :-
   write('teste'), nl,
   nth(Y, Board, Row),
   nth(X, Row, CurrentPiece),
-  write('X: '), write(X), write(' | Y: '), write(Y), write(' | CP: '), write(CurrentPiece), nl,
-  name(CurrentPiece, Piece), write(CurrentPiece), nl.
+  name(CurrentPiece, Piece).
 
-is_inside_board(X, Y) :- 
+is_inside_board(X, Y) :- % Verify if Piece is inside the game board
   Min is 1, Max is 8,
   (X >= Min, X =< Max), (Y >= Min, Y =< Max).
